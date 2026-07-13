af1cb38a9e3dcd51327484170fc854619f57db3a
vitor <vitor.collis@gmail.com>
2026-07-13 12:39:40 -0300

    Inserção de usuário com POST

--- src/main/java/com/vitorcollis/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/vitorcollis/workshopmongo/dto/UserDTO.java
@@ -19,14 +19,7 @@ public class UserDTO implements Serializable {
 		name = obj.getName();
 		email = obj.getEmail();
 	}
-
-	public UserDTO(String id, String name, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-	}
-
+	
 	public String getId() {
 		return id;
 	}
--- src/main/java/com/vitorcollis/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vitorcollis/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.vitorcollis.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,17 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitorcollis.workshopmongo.domain.User;
 import com.vitorcollis.workshopmongo.dto.UserDTO;
 import com.vitorcollis.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value ="/users")
+@RequestMapping(value="/users")
 public class UserResource {
+
 	@Autowired
 	private UserService service;
 	
@@ -27,11 +31,18 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 
+	@RequestMapping(method=RequestMethod.POST)
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
